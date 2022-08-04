4f7fca1be91f43cea2301fc6027281446121a3c8
John Kennedy Kalu <72397673+kvngdre@users.noreply.github.com>
2022-08-04 09:51:38 +0100

    implemented automation

--- engine.py
+++ engine.py
@@ -1,4 +1,48 @@
-from dotenv import load_dotenv
 import os
+import requests
+from dotenv import load_dotenv
+from urllib.parse import quote_plus
 
 load_dotenv()
+
+url = "https://api.ebulksms.com:4433/sendsms?username={0}&apikey={1}&sender={2}&messagetext={3}&flash={4}&recipients={5}"
+
+
+def format_phone_numbers(numbers):
+    num_list = [num for num in numbers if len(num) > 10]
+
+    num_list = [num if num[:3]=="234" else ("234" + num[-9:] if len(num)==10 else "234" + num[-10:]) for num in num_list]
+    
+    result = ",".join(num_list)
+
+    return result
+
+
+def encode_values(sender_id, message, recipients):
+    user_name = quote_plus(os.environ.get('USER_NAME'))
+    api_key = quote_plus(os.environ.get('API_KEY'))
+    recipients = quote_plus(recipients)
+    sender = quote_plus(sender_id)
+    message = quote_plus(message)
+
+    return user_name, api_key, sender, message, recipients
+
+
+def send_message(sender_id, msg, recipients):
+    formatted_numbers = format_phone_numbers(recipients)
+    user_name, api_key, sender, message, recipients = encode_values(sender_id, msg, formatted_numbers)
+
+    print(user_name, api_key)
+
+    formatted_url = url.format(user_name,api_key,sender,message,0,recipients)
+    print(formatted_url)
+
+    try:
+        response = requests.request("GET", formatted_url)
+
+        return response.text
+
+    except Exception as error:
+        print(error)
+        return error
+
--- main.py
+++ main.py
@@ -1,1 +1,40 @@
-print('Hello world')
+from nbformat import write
+import pandas as pd
+import streamlit as st
+import engine
+
+st.header('EBULK SMS AUTOMATION')
+
+phone_numbers = st.file_uploader('Upload file', ['xlsx'])
+if phone_numbers is not None:
+    try:
+        df = pd.read_excel(phone_numbers, dtype={'Phone': str})
+        numbers_list = list(df.Phone)
+    except AttributeError as error:
+        st.error(error)
+        st.stop()
+
+    col1, col2, col3 = st.columns([1, 3, 4])
+    with col1:
+        st.write('Row count:')
+    with col2:
+        st.write(df.shape[0])
+
+
+st.write('\n')
+with st.form('bulk_sms'):
+    col3, col4 = st.columns([1, 3])
+
+    with col3:
+        sender_id = st.text_input('Enter sender id:')
+
+    message_body = st.text_area('Type message here', max_chars=612)
+
+
+    submitted = st.form_submit_button('Submit')
+
+if submitted:
+    with st.spinner():
+        response = engine.send_message(sender_id, message_body, numbers_list)
+    st.info(response)
+    
